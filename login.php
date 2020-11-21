489af48b7cd63e09a82adde3ea8b4f83ed8fb621
StrangeOrange <45105332+StrangeOrange@users.noreply.github.com>
2020-11-21 18:30:07 +0300

    login

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
     <title>Document</title>
 </head>
 <body>
-<form class="form-3">
+<form class="form-3" action="auth.php" method="post">
     <p class="clearfix">
         <label for="login">Логин</label>
         <input type="text" name="login" id="login" placeholder="Логин">
